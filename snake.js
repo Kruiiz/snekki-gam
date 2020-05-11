b7351d234a2df93ab7e3b1ef567a5b6ae83626db
Kruiiz <42588014+Kruiiz@users.noreply.github.com>
2020-05-10 17:34:56 -0700

    Update snake.js

--- snake.js
+++ snake.js
@@ -1,30 +1,40 @@
 class Snake {
   
   constructor() {
-  	this.body = [];
+    this.body = [];
     this.body[0] = createVector(floor(w/2), floor(h/2));
     this.xdir = 0;
     this.ydir = 0;
     this.len = 0;
+    this.num =10
+    this.bg= [this.getRandomInt(), this.getRandomInt(), this.getRandomInt()] 
   }
   
   setDir(x, y) {
   	this.xdir = x;
     this.ydir = y;
   }
+   
+getRandomInt() {
+    min = Math.ceil(0);
+    max = Math.floor(200);
+    return Math.floor(Math.random() * (200 - 0 + 1)) + 0;
+}
   
   update() {
   	let head = this.body[this.body.length-1].copy();
     this.body.shift();
     head.x += this.xdir;
     head.y += this.ydir;
-    this.body.push(head);
+    this.body.push(head); 
   }
   
-  grow() {
+  grow(num) {
   	let head = this.body[this.body.length-1].copy();
     this.len++;
     this.body.push(head);
+    frameRate(this.num + 3)
+    this.num= this.num + 3
   }
   
   endGame() {
@@ -43,10 +53,12 @@ class Snake {
   }
   
   eat(pos) {
-  	let x = this.body[this.body.length-1].x;
+    background(this.bg)  
+    let x = this.body[this.body.length-1].x;
     let y = this.body[this.body.length-1].y;
     if(x == pos.x && y == pos.y) {
-      this.grow();
+      this.bg= [this.getRandomInt(), this.getRandomInt(), this.getRandomInt()]
+      this.grow(20);
       return true;
     }
     return false;
@@ -57,7 +69,6 @@ class Snake {
     	fill(0);
       noStroke();
       rect(this.body[i].x, this.body[i].y, 1, 1)
-    }
+    }    
   }
-
 }
